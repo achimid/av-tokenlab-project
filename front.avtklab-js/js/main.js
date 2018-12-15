61deb7ad25e848569f5a9c8fdee8fc2c08580129
Achimid <achimid@hotmail.com>
2018-12-15 18:34:08 -0200

    criando uma pasta do layout para implementar uma versao somente com js, para inicio

---
+++ front.avtklab-js/js/main.js
@@ -0,0 +1,96 @@
+
+(function ($) {
+    // USE STRICT
+    "use strict";
+    $(".animsition").animsition({
+      inClass: 'fade-in',
+      outClass: 'fade-out',
+      inDuration: 900,
+      outDuration: 900,
+      linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])',
+      loading: true,
+      loadingParentElement: 'html',
+      loadingClass: 'page-loader',
+      loadingInner: '<div class="page-loader__spin"></div>',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: ['animation-duration', '-webkit-animation-duration'],
+      overlay: false,
+      overlayClass: 'animsition-overlay-slide',
+      overlayParentElement: 'html',
+      transition: function (url) {
+        window.location.href = url;
+      }
+    });
+  
+  
+  })(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  // Load more
+  try {
+    var list_load = $('.js-list-load');
+    if (list_load[0]) {
+      list_load.each(function () {
+        var that = $(this);
+        that.find('.js-load-item').hide();
+        var load_btn = that.find('.js-load-btn');
+        load_btn.on('click', function (e) {
+          $(this).text("Loading...").delay(1500).queue(function (next) {
+            $(this).hide();
+            that.find(".js-load-item").fadeToggle("slow", 'swing');
+          });
+          e.preventDefault();
+        });
+      })
+
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  try {
+    
+    $('[data-toggle="tooltip"]').tooltip();
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Chatbox
+  try {
+    var inbox_wrap = $('.js-inbox');
+    var message = $('.au-message__item');
+    message.each(function(){
+      var that = $(this);
+
+      that.on('click', function(){
+        $(this).parent().parent().parent().toggleClass('show-chat-box');
+      });
+    });
+    
+
+  } catch (error) {
+    console.log(error);
+  }
+
+})(jQuery);
+
+
+$(function() {
+  $('.datepicker').datepicker({
+    todayBtn: "linked",
+    locale: 'pt-br',
+    autoclose: true,
+    todayHighlight: true,
+    dateFormat: 'dd/mm/yy' 
+  });  
+});
